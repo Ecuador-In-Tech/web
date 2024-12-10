9767ad3114f7b56470f25f48bcc55cded59183d0
Brayen Gavilanes <bjgavilanes@gmail.com>
2024-12-10 00:02:34 -0300

    ci: dprint fmt && biome check --write

--- src/interfaces/communities.ts
+++ src/interfaces/communities.ts
@@ -1,12 +1,12 @@
 export interface Community {
-    name: string,
-    description: string,
-    links: Links,
-    tags: string[],
-    image: string
+	name: string;
+	description: string;
+	links: Links;
+	tags: string[];
+	image: string;
 }
 
-export interface Links{
-    website: string, 
-    social: string
+export interface Links {
+	website: string;
+	social: string;
 }
